170bd3c55b3e9193f096e57ad767896c38528202
Peter Strömberg <pez@pezius.com>
2023-05-14 21:27:31 +0200

    Use Babashka instead of bash and clojure

---
+++ docker/cheap-deps.clj
@@ -0,0 +1,25 @@
+#!/usr/bin/env bb
+
+(require '[clojure.string :as string])
+(require '[clojure.edn :as edn])
+(require '[babashka.deps :as deps])
+(require '[babashka.classpath :as classpath])
+(require '[babashka.fs :as fs])
+
+(defn process-classpath [classpath]
+  (->> (classpath/split-classpath classpath)
+       (map (fn [path]
+              (if (clojure.string/ends-with? path ".jar")
+                (let [deps-subdir (string/replace (fs/file-name path) #".jar$" "")
+                      deps-dir (str "/app/dependencies/" deps-subdir)]
+                  (fs/create-dirs deps-dir)
+                  (fs/unzip path deps-dir {:replace-existing true})
+                  deps-dir)
+                path)))
+       (string/join ":")))
+
+(when (= *file* (System/getProperty "babashka.file"))
+  (deps/add-deps (edn/read-string (slurp "deps.edn")) {:aliases [:dev]})
+  (-> (classpath/get-classpath)
+      process-classpath
+      println))
--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir -p /app/dependencies
-CLOJURE_LOAD_PATH=$(/app/docker/cheap-deps.sh)
+CLOJURE_LOAD_PATH=$(bb /app/docker/cheap-deps.clj)
 echo "export CLOJURE_LOAD_PATH=${CLOJURE_LOAD_PATH}" > /app/dependencies/load-path.sh
 
 exec "$@"
